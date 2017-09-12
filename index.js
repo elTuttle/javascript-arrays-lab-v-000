d615730fed2575f0b7076742bf71d13916e3e0e6
John Tuttle <thejwt@gmail.com>
2017-09-12 00:59:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,14 +28,16 @@ function prependKitten(name){
 }
 
 function removeFirstKitten(){
-  var newKittens = [kittens.splice(1)]
+  var oldKittens = new array[]
+  oldKittens = kittens
+  var newKittens = kittens.splice(1)
   console.log(newKittens);
   console.log(kittens);
   return newKittens
 }
 
 function removeLastKitten(){
-  var newKittens = [kittens.splice(0,kittens.length - 1)]
+  var newKittens = kittens.splice(0,kittens.length - 1)
   console.log(newKittens);
   console.log(kittens);
   return newKittens
