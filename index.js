66a37a5e4893d0a9b336f44879b75e4dcb4f3df0
John Tuttle <thejwt@gmail.com>
2017-09-12 01:01:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ function removeFirstKitten(){
 }
 
 function removeLastKitten(){
-  var newKittens = kittens.splice(0,kittens.length - 1)
+  var newKittens = kittens.shift()
   console.log(newKittens);
   console.log(kittens);
   return newKittens
