f68b9040de1429340df70222cbcdf3c98c2968df
John Tuttle <thejwt@gmail.com>
2017-09-12 01:03:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ function prependKitten(name){
 }
 
 function removeFirstKitten(){
-  var newKittens = [..kittens]
+  var newKittens = [...kittens]
   newKittens.pop()
   return newKittens
 }
 
 function removeLastKitten(){
-  var newKittens = [..kittens]
+  var newKittens = [...kittens]
   newKittens.shift()
   return newKittens
 }
